001bd59027a9696416d36181225222a1b48d40ef
felipemimoura <felipemimoura@gmail.com>
2022-01-17 06:37:05 -0300

    feat: finalizacao do modulo 01

---
+++ 02_operacoesAritimeticas.js
@@ -0,0 +1,18 @@
+console.log("02 - Operacacoes aritimeticas");
+
+const anoNascimento = 1994;
+const anoAtual = 2022;
+
+
+const idade = anoAtual - anoNascimento;
+console.log(idade)
+
+const anoAtualEmString = "2022" 
+const anoNascimentoEmString = "1994"
+const idadeComAnoEmString = anoAtualEmString - anoNascimento
+
+console.log(idadeComAnoEmString)
+const idadeComAnoAtualEAnoNascimentmEmString = anoAtualEmString - anoNascimento
+console.log(idadeComAnoAtualEAnoNascimentmEmString)
+
+

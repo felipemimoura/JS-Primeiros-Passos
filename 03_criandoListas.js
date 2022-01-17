7b3eda2e5c81af43d6e5bca3902ee118316694a9
felipemimoura <felipemimoura@gmail.com>
2022-01-17 07:02:10 -0300

    feat: criacao da primeira lista e adiconado elementos de maneira dinamica

---
+++ 03_criandoListas.js
@@ -0,0 +1,18 @@
+console.log(`Trabalhando com listas`);
+
+
+// [] criar uma lista
+//      [] cria a lista utilizando a sintaxe new Array
+//      [] cria a lista com os []
+//      [] adiconar um elemento dentro da lista com push
+
+const listaDeDestinos = new Array (
+    `Salvador`,
+    `Sao Paulo`,
+    `Rio de Janeiro`
+)
+console.log(listaDeDestinos)
+
+//Adicionando um elemento na lista
+listaDeDestinos.push(`Curitiba`)
+console.log("Lista com elementos adicionado apos ser inicializado",listaDeDestinos)

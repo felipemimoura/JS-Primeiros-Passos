5dca04a9654bc90860b56a3694d881a29e924c2c
Felipe Moura <felipe.moura@interprocess.com.br>
2022-01-21 14:30:20 -0300

    feat: adiciona condiconais para verificar idade ou se o comprador está acompanhado

--- 04_condicionais.js
+++ 04_condicionais.js
@@ -1,7 +1,9 @@
-// [] Criar lista de destinos
-// [] Vender um destino somente para comprador maior de 18
-// [] Comprador menor de idade mostar mensagem
-// [] Exibir lista de destino atulizaddos 
+// [x] Criar lista de destinos
+// [x] Vender um destino somente para comprador maior de 18
+// [x] Comprador menor de idade mostar mensagem
+// [x] Exibir lista de destino atulizaddos 
+// [] verificar se o comparador está acompanhado
+// [] Se acompanhado pode vender
 
 console.log("Trabalhando com condicionais")
 const listaDeDestinos = new Array(
@@ -9,17 +11,20 @@ const listaDeDestinos = new Array(
     `Sao Paulo`,
     `Rio de Janeiro`
 )
-const idadeComprador = 18
+const idadeComprador = 17
+const estaAcompnhado = true
 
 console.log("Lista de destinos disponiveis")
 console.log(listaDeDestinos)
 
-if (idadeComprador >= 18) {
-    console.log(`O comprador tem ${idadeComprador} anos e pode comprar`)
+if (idadeComprador >= 18 || estaAcompnhado) {
+
+    console.log(`O comprador tem ${idadeComprador} anos ou está companhado`)
     console.log(`O destino selecionado foi ${listaDeDestinos[1]}`)
     listaDeDestinos.splice(1, 1)
+
 } else {
-    console.log(`O comprador tem ${idadeComprador} anos e não posso vender`)
+    console.log(`O comprador tem ${idadeComprador} anos ou não está acompanhado. Então não posso vender`)
 }
 
 
